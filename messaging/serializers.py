5ff69f5f6116353a039be2a7d096827aebc631be
Arishaba-Peruth <lorisperutha@gmail.com>
2026-02-09 21:04:39 +0300

    Updated the serializers.py allowing language to use data.get and corrected swapped fields

--- messaging/serializers.py
+++ messaging/serializers.py
@@ -2,14 +2,19 @@ from rest_framework import serializers
 from .models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
+ translated_text = serializers.CharField(source='translated_text', read_only=True)
+ 
  class Meta:
      model = Message
      fields = "__all__"
 
  def create(self, validated_data):
      return Message.objects.create(
-         sender=validated_data['recipient'],
-         recipient=validated_data['sender'],
+         #sender=validated_data['recipient'],
+         sender=validated_data['sender'],
+         #recipient=validated_data['sender'],
+         recipient=validated_data['recipient'],
          text=validated_data['text'],
-         language=validated_data['language']
+         #language=validated_data['language']
+         language=validated_data.get('language', 'en')
      )
