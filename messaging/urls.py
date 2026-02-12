0c1ed97f7040aeea65babdb9f3f34497127ac47a
Arishaba-Peruth <lorisperutha@gmail.com>
2026-02-12 17:45:37 +0300

    Created a delete message API endpoint

--- messaging/urls.py
+++ messaging/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/messages/', views.MessageCreateView.as_view(), name='message-create'),  
-    path('api/messages/inbox/', views.InboxView.as_view(), name='message-list'),
-    path('api/messages/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message-delete'),
+    path('messages/', views.MessageCreateView.as_view(), name='message-create'),  
+    path('messages/inbox/', views.InboxView.as_view(), name='message-list'),
+    path('messages/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message-delete'),
 ]
